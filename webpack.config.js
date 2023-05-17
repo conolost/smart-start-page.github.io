6031be15d7ef9d1abf0d08f24d2dc00feb84fda1
conolost <conolost@gmail.com>
2023-05-17 17:12:10 +0300

    fixed(webpack-config): remove css link in the index

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      // {
-      // test: /\.s(a|c)ss$/,
-      // use: ["style-loader", "css-loader", "sass-loader"],
-      // },
+      {
+        test: /\.s(a|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
       // {
       //   test: /\.css$/,
       //   use: ["style-loader", "css-loader"],
